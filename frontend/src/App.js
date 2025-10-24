55f0b14570419cc7a1284f30a583a31db2836d01
Koushik-31368 <2300031368@kluniversity.in>
2025-10-25 04:41:10 +0530

    Add CORS to AuthController

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,9 +9,8 @@ import RegisterPage from './pages/RegisterPage';
 import ForgotPasswordPage from './pages/ForgotPasswordPage';
 import LobbyPage from './pages/LobbyPage';
 import GamePage from './pages/GamePage';
-// ... other imports
 import StrategiesPage from './pages/StrategiesPage';
-import SimulatorPage from './pages/SimulatorPage'; // 1. Import
+import SimulatorPage from './pages/SimulatorPage'; // For the simulator project
 
 function App() {
   return (
@@ -27,7 +26,7 @@ function App() {
             <Route path="/multiplayer" element={<LobbyPage />} />
             <Route path="/game/:gameId" element={<GamePage />} />
             <Route path="/strategies" element={<StrategiesPage />} />
-            <Route path="/simulator" element={<SimulatorPage />} /> {/* 2. Add route */}
+            <Route path="/simulator" element={<SimulatorPage />} />
           </Routes>
           <StockTicker />
         </div>
@@ -35,4 +34,5 @@ function App() {
     </AuthProvider>
   );
 }
+
 export default App;
